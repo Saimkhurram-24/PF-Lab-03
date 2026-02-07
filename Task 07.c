ee11a7aad62d9ccc5b3c0832b7b031372b43e5f4
Saimkhurram-24 <k256500@nu.edu.pk>
2026-02-07 20:09:55 +0500

    Create Task 07.c

---
+++ Task 07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+using namespace std;
+int main() {
+    int a = 10;
+    float b = 3.5;
+    double c = 9.87654;
+    char d = 'S';
+
+    printf("Integer: %d\n", a);
+    printf("Float: %f\n", b);
+    printf("Double: %lf\n", c);
+    printf("Character: %c\n", d);
+
+    return 0;
+}
